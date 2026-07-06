c4af6f93315d11f55e0e762bbf90099691b10878
Dal0328 <dhkh1111@snu.ac.kr>
2026-07-06 20:59:24 +0900

    feat: sparse_adam

--- setup.py
+++ setup.py
@@ -15,16 +15,17 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_surfel_rasterization_abs",
-    packages=['diff_surfel_rasterization_abs'],
+    name="diff_surfel_rasterization_fisheye",
+    packages=['diff_surfel_rasterization_fisheye'],
     version='0.0.0',
     ext_modules=[
         CUDAExtension(
-            name="diff_surfel_rasterization_abs._C",
+            name="diff_surfel_rasterization_fisheye._C",
             sources=[
             "cuda_rasterizer/rasterizer_impl.cu",
             "cuda_rasterizer/forward.cu",
             "cuda_rasterizer/backward.cu",
+            "cuda_rasterizer/adam.cu",
             "rasterize_points.cu",
             "ext.cpp"],
             extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
